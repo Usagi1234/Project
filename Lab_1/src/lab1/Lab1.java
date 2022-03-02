4a3a7437347c8af66d3ce9b08b0b981b50bfc75c
noino <noino@DESKTOP-BGHRIJK>
2022-03-03 00:03:05 +0700

    LAB02

---
+++ Lab_1/src/lab1/Lab1.java
@@ -0,0 +1,13 @@
+package lab1;
+
+public class Lab1 {
+
+	public Lab1() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
